0237aecd766aeb45b42d3e97c177d9ccfebd3c05
Axel Schouten <schouten.axel@gmail.com>
2024-04-24 23:10:49 +0200

    Update final backend folder fastapi:

---
+++ backend/src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def health():
+    return "Super Cool"
+
+@app.get('/test')
+async def test():
+    return "TEST"
+
+@app.get('/axel')
+async def test():
+    return "axel"
